39a50fe0bb106d7ce28696c198f559c3bd864613
树海 <yangzq007@126.com>
2019-08-08 14:12:53 +0800

    feat: 添加配置代码段逻辑

--- bin/snippet.js
+++ bin/snippet.js
@@ -1,10 +1,72 @@
+#!/usr/bin/env node
+
 const execSync = require('child_process').execSync;
+const readline = require('readline');
+const fs = require('fs');
+const os = require('os');
+
+var userTag = "YCode";
+var key = "y";
+
+const userTagPlaceholder = /\$\$userTag\$\$/;
+const keyPlaceholder = /\$\$key\$\$/;
+
+function answer2Question(questtion) {
+
+  return new Promise((resolve, reject) => {
+    var rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    rl.question(questtion, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+function download() {
+
+  console.log("ycode delete old codeSnippets ...")
+  execSync("rm -rf ~/Library/Developer/Xcode/UserData/CodeSnippets");
+
+  console.log("ycode clone new codeSnippets ...");
+  execSync("cd ~/Library/Developer/Xcode/UserData/ && git clone https://github.com/yangzq007/CodeSnippets.git");
+  //debug code
+  // execSync("cd ~/Library/Developer/Xcode/UserData/ && git clone https://github.com/yangzq007/CodeSnippets.git  && cd CodeSnippets && git checkout develop");
+
+  config();
+}
+
+function config() {
+
+  console.log('ycode start config ...');
+
+  const p = `${os.homedir()}/Library/Developer/Xcode/UserData/CodeSnippets`;
+  const files = fs.readdirSync(p);
+  files.forEach((item, index) => {
+    if (item.indexOf(".codesnippet") != -1) {
+      const filePath = `${p}/${item}`;
+      const data = fs.readFileSync(filePath);
+      const str = data.toString().replace(userTagPlaceholder,userTag).replace(keyPlaceholder,key);
+      fs.writeFileSync(filePath,str); 
+    }
+  });
+
+  console.log('ycode success!')
+}
 
 const snippet = function snippet() {
-    console.log("ycode delete old codeSnippets ...")
-    execSync("rm -rf ~/Library/Developer/Xcode/UserData/CodeSnippets");
 
-    console.log("ycode clone new codeSnippets ...");
-    execSync("cd ~/Library/Developer/Xcode/UserData/ && git clone https://github.com/yangzq007/CodeSnippets.git");
+  answer2Question('userTag:').then(res => {
+    if (res != "") {
+      userTag = res; 
+    }
+    return answer2Question("key:");
+  }).then(res => {
+    if (res != "") {
+      key = res; 
+    }
+    download();
+  }).catch(error => {
+    console.log(`readline error: ${error}`);
+  });
 }
 module.exports = snippet;
